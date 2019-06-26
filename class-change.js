c4d53c9e33bde4fef1ff43c79c351b905aa3d2f4
goates-vanessa <43688666+goates-vanessa@users.noreply.github.com>
2019-06-25 20:53:17 -0600

    Add files via upload

---
+++ class-change.js
@@ -0,0 +1,47 @@
+function changeCss(x) {
+    var a = document.getElementsByClassName("arrow"); 
+    for (var i= 0; i<a.length; i++){
+        if(i==x)
+        continue; 
+        a[i].className = "arrow";
+    }
+    if(a[x].className=="arrow arrowdown"){
+      a[x].className="arrow"; 
+    }else {
+     a[x].className="arrow arrowdown"; 
+    }
+    var y = document.getElementsByTagName("ul");
+    for (var i=0; i<y.length; i++ ) {
+        if(i==x)
+        continue; 
+
+        y[i].className="noshow";
+    }
+
+    if(y[x].className=="yesshow"){
+        y[x].className= "noshow"; 
+    }else {
+        y[x].className = "yesshow"; 
+    }
+   
+}
+function changebackgroundcolor() {
+    var z= document.getElementsByClassName("background-style")
+    for (var i=0; i<z.length; i++){
+        if (z[i].style.backgroundColor== "#4d79ff"){
+            z[i].style.backgroundColor= "#00ffff";
+        }
+        else {
+        z[i].style.backgroundColor = "#4d79ff"; 
+        }
+    }
+}
+var shoes, list; 
+shoes =["heels", "tennis shoes", "sandals", "pumas"];
+
+function showArray(){
+    document.getElementById("length").innerHTML = shoes.length; 
+    document.getElementById("firstelement").innerHTML = shoes[0];
+    document.getElementById("secondelement").innerHTML = shoes[1];
+    document.getElementById("fourthelement").innerHTML = shoes[3];
+}
