6ef7c64a63f24d73c9e1580447697b6794a9ba41
Vanessa <anng@Vanessas-MacBook-Air.local>
2019-07-03 22:00:57 -0600

    localstorage changes

--- local-storage/scripts/storage.js
+++ local-storage/scripts/storage.js
@@ -1,13 +1,13 @@
 document.getElementById("exTwo").innerHTML = "My full name is:" + localStorage.user.fullName();
 
 function showArray(){
-    document.getElementById("length").innerHTML = localStorage.favorites.length; 
-    document.getElementById("firstelement").innerHTML = localStorage.favorites[0];
-    document.getElementById("secondelement").innerHTML = localStorage.favorites[1];
-    document.getElementById("fourthelement").innerHTML = localStorage.favorites[3];
+    document.getElementById("length").innerHTML = localStorage.getItem("favorites").length; 
+    document.getElementById("firstelement").innerHTML = localStorage.getItem("favorites")[0];
+    document.getElementById("secondelement").innerHTML = localStorage.getItem("favorites")[1];
+    document.getElementById("fourthelement").innerHTML = localStorage.getItem("favorites")[3];
 }
 
-document.getElementById("exOnea").innerHTML = localStorage.counter["one"];
+document.getElementById("exOnea").innerHTML = localStorage.getItem("counter")["one"];
 
 document.getElementById("exOneb").innerHTML = localStorage.counter["two"];
 
